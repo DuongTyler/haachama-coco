8f31df12255fb881f324d2d3977be9a7887d2262
Zapple <zizibaluba@gmail.com>
2020-10-11 11:36:56 -0700

    add animation response and sort by Alpha

--- frontend/src/models/response.ts
+++ frontend/src/models/response.ts
@@ -1,28 +1,33 @@
-import {MessageJson} from "./message";
-import {GameJson} from "./game";
-import {ArtworkJson} from "./artwork";
+import {AnimationJson} from "./animation";
 import {ArchiveJson} from "./archive";
+import {ArtworkJson} from "./artwork";
+import {GameJson} from "./game";
+import {MessageJson} from "./message";
 
 interface BaseResponse {
     status: string;
-}
-
-export interface MessageResponse extends BaseResponse {
-    messages: MessageJson[];
-}
-
-export interface GamesResponse extends BaseResponse {
-    games: GameJson[];
-}
-
-export interface GalleryResponse extends BaseResponse {
-    gallery: ArtworkJson[];
 }
 
 export interface ArchiveResponse extends BaseResponse {
     archives: ArchiveJson[];
 }
 
+export interface AnimationResponse extends BaseResponse {
+    archives: AnimationJson[];
+}
+
 export interface CountResponse extends BaseResponse {
     count: number;
+}
+
+export interface GalleryResponse extends BaseResponse {
+    gallery: ArtworkJson[];
+}
+
+export interface GamesResponse extends BaseResponse {
+    games: GameJson[];
+}
+
+export interface MessageResponse extends BaseResponse {
+    messages: MessageJson[];
 }
